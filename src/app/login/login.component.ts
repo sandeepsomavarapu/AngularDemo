9b5c60845357bcfc13432ec0d457580058359216
sandeep <sandeepsomavarapu535@gmail.com>
2022-11-16 14:08:33 +0530

    validation

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -8,10 +8,22 @@ import { Component, OnInit } from '@angular/core';
 export class LoginComponent implements OnInit {
 
   orgName:string="Angular";
+  uname:string;
+  pass:string;
 
   constructor() { }
 
   ngOnInit(): void {
   }
-
+  validate(loginform:any){
+     this.uname=loginform.value.uname;
+     this.pass=loginform.value.pswd;
+     if(this.uname=="accenture"&& this.pass=="accenture123")
+     {
+      alert("Login Success");
+     }
+     else{
+      alert("Enter Valid Credentials");
+     }
+  }
 }
