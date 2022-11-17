7742927bcce9ef20766878a7010616f47f2ec5e5
sandeep <sandeepsomavarapu535@gmail.com>
2022-11-17 11:30:43 +0530

    day-4 observable

--- src/app/register/register.component.ts
+++ src/app/register/register.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { filter, Observable, Subscription } from 'rxjs';
 
 @Component({
   selector: 'register',
@@ -8,8 +9,37 @@ import { Component, OnInit } from '@angular/core';
 export class RegisterComponent implements OnInit {
 
   constructor() { }
+private subscription:Subscription;
+  ngOnInit() {
+    //promise
+    const promise = new Promise(resolve => {
+      console.log("promise call.....");
+      setTimeout(() => {
+        resolve('promise is working');
+        resolve('promise is working1');
+        resolve('promise is working2');
+        resolve('promise is working3');
 
-  ngOnInit(): void {
+      }, 1000
+      )
+    });
+    promise.then(result => console.log(result))
+    //Observable
+    const observable = new Observable(sub => {
+      console.log("observable call...")
+      let counter=0;
+      setInterval(() => {
+        counter=++counter;
+        sub.next(counter);
+         }, 1000
+      )
+    });
+   this.subscription= observable.subscribe(result => console.log("subscriber counte"+result))
+
   }
+
+ngOnDestroy(){
+this.subscription.unsubscribe();
+}
 
 }
